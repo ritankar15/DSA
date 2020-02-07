63e6af175bf6129aeee3d08a6a4001fb88ff3373
ritankar15 <60777433+ritankar15@users.noreply.github.com>
2020-02-07 12:52:27 +0000

    Create DS.java

---
+++ DS.java
@@ -0,0 +1,20 @@
+package dsandalg;
+
+import datastructures.SinglyLinkedList;
+
+public class DS {
+
+	public static void main(String[] args) {
+		SinglyLinkedList s = new SinglyLinkedList(2);
+		s.addSinglyLinkedListRear(4);
+		s.addSinglyLinkedListRear(6);
+		s.addSinglyLinkedListRear(8);
+		s.addSinglyLinkedListRear(10);
+		s.addSinglyLinkedListRear(12);
+		s.addSinglyLinkedListRear(14);
+		s.displaySinglyLinkedList();
+		s.replaceElementSinglyLinkedList(4, 3);
+		s.displaySinglyLinkedList();
+	}
+
+}
